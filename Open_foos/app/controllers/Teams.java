a93415d4b5733e8199528baa9208da73a509bdab
ag12 <amirg12@hotmail.no>
2012-04-10 13:40:38 +0200

    controller

---
+++ Open_foos/app/controllers/Teams.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ *
+ * @author Santonas
+ */
+public class Teams extends Controller {
+    
+}
