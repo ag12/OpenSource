dc7d3a99601bcb4a246b978bf7aca6232083b9ad
ag12 <amirg12@hotmail.no>
2012-04-25 13:39:55 +0200

    test_packes

---
+++ Open_foos/test/Players/PlayersFunctionalTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Players;
+
+import play.test.FunctionalTest;
+
+/**
+ *
+ * @author Santonas
+ */
+public class PlayersFunctionalTest extends FunctionalTest {
+    
+}
---
+++ Open_foos/test/Players/PlayersUnitTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Players;
+
+import play.test.UnitTest;
+
+/**
+ *
+ * @author Santonas
+ */
+public class PlayersUnitTest extends UnitTest {
+    
+}
